ab2bbe478a6d6b5155710d499b821fbc286735f9
chrismarksus <chrismarksus@gmail.com>
2022-10-01 23:11:34 -0400

    2 files modified, 4 added

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // run `node index.js` in the terminal
+function getFinalPrice(price: number, discount: number) {
+  return price - price/discount;
+}
+ 
+console.log(getFinalPrice(100, 10));
+console.log(getFinalPrice(100, "10%"));
 
 console.log(`Hello Node.js v${process.versions.node}!`);
---
+++ main.ts
@@ -0,0 +1,13 @@
+function calcTax(
+  state: string,
+  income: number,
+  dependents: number
+): number | undefined {
+  if (state === 'NY') {
+    return income * 0.06 - dependents * 500;
+  } else if (state === 'NJ') {
+    return income * 0.05 - dependents * 300;
+  }
+}
+
+let tax = calcTax('NJ', 50000, 2);
